7b5c758cef7b05f7cb4004310907c9da26aa4ba4
kaustubh <kaustubh229@gmail.com>
2021-05-29 14:09:51 +0530

    --Improvements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { useEffect } from 'react'
-import Particles from './components/layouts/Particles'
-import Header from './components/pages/Header'
-import About from './components/pages/About'
-import Works from './components/pages/Works'
-import Contact from './components/pages/Contact'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { animation } from './profile'
-
+import React, { useEffect } from "react";
+import Particles from "./components/layouts/Particles";
+import Header from "./components/section/Header";
+import About from "./components/section/About";
+import Works from "./components/section/Works";
+import Contact from "./components/section/Contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { animation } from "./profile";
 
 function App() {
-
   useEffect(() => {
-      AOS.init({
-        duration: animation.duration,
-        once: animation.once,
-        disable: !animation.animate
-      })
-// eslint-disable-next-line
-  }, [])
+    AOS.init({
+      duration: animation.duration,
+      once: animation.once,
+      disable: !animation.animate,
+    });
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <div className="App">
--- src/profile.js
+++ src/profile.js
@@ -1,168 +1,175 @@
 // Change website animations
 const animation = {
-    // make it false to switch off fade-up animation
-    animate: true,
-    // animation playing duration
-    duration: 750,
-    // if false, animation plays everytime element comes on screen
-    once: true
-}
+  // make it false to switch off fade-up animation
+  animate: true,
+  // animation playing duration
+  duration: 750,
+  // if true, animation plays only once when element comes on screen
+  once: false,
+};
 // Change your display name on tha landing display
 const header = {
-    name: "a Human",
-}
+  name: "a Human",
+};
 const background = {
-    // Options: Snow or Particle
-    type: "Snow"
-}
+  // Options: Snow or Particle
+  type: "Snow",
+};
 // Write a para about yourself here
 // To update your image, go to './styles/images.css'
-const section2title = "About Me"
+const section2title = "About Me";
 const about = {
-    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. Orci phasellus egestas tellus rutrum tellus pellentesque eu. Dapibus ultrices in iaculis nunc. Pretium nibh ipsum consequat nisl vel. Consequat ac felis donec et odio. Orci nulla pellentesque dignissim enim sit amet. Scelerisque eu ultrices vitae auctor eu augue ut lectus arcu."
-}
+  paragraph:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. ",
+};
 // Edit your skill and the percentage you know about it
 // To Add a skill, copy any one below and paste it after the last comma
 const skillsBar = [
-    {
-        name: "HTML5",
-        faClass: "fab fa-html5"
-    },{
-        name: "CSS3",
-        faClass: "fab fa-css3"
-    },{
-        name: "Javascript",
-        faClass: "fab fa-js"
-    },{
-        name: "SASS",
-        faClass: "fab fa-sass"
-    },{
-        name: "Node",
-        faClass: "fab fa-node"
-    },{
-        name: "Python",
-        faClass: "fab fa-python"
-    },{
-        name: "Java",
-        faClass: "fab fa-java"
-    },{
-        name: "PHP",
-        faClass: "fab fa-php"
-    },{
-        name: "Database",
-        faClass: "fas fa-database"
-    },{
-        name: "AWS",
-        faClass: "fab fa-aws"
-    },{
-        name: "Docker",
-        faClass: "fab fa-docker"
-    },{
-        name: "NPM",
-        faClass: "fab fa-npm"
-    },{
-        name: "Heroku",
-        faClass: "fas fa-h-square"
-    },{
-        name: "Swift",
-        faClass: "fab fa-swift"
-    },{
-        name: "Linux",
-        faClass: "fab fa-linux"
-    },{
-        name: "Wordpress",
-        faClass: "fab fa-wordpress"
-    }
-]
-// Edit your projects, its name, your skills used to make it, and the url. 
+  {
+    name: "HTML5",
+    faClass: "fab fa-html5",
+  },
+  {
+    name: "CSS3",
+    faClass: "fab fa-css3",
+  },
+  {
+    name: "Javascript",
+    faClass: "fab fa-js",
+  },
+  {
+    name: "SASS",
+    faClass: "fab fa-sass",
+  },
+  {
+    name: "Node",
+    faClass: "fab fa-node",
+  },
+  {
+    name: "Python",
+    faClass: "fab fa-python",
+  },
+  {
+    name: "Java",
+    faClass: "fab fa-java",
+  },
+  {
+    name: "PHP",
+    faClass: "fab fa-php",
+  },
+  {
+    name: "Database",
+    faClass: "fas fa-database",
+  },
+  {
+    name: "AWS",
+    faClass: "fab fa-aws",
+  },
+];
+// Edit your projects, its name, your skills used to make it, and the url.
 // You can omit freely anything if you dont have it
 // To Add a Project, copy any one below and paste it after the last comma and increment the id's project number
-const section3Title = "Past Projects"
+const section3Title = "Past Projects";
 const projects = [
-    {
-        // Add image in './styles/images.css' in #project1
-        id: "project1",
-        name: "Project 1",
-        skills: ["HTML, CSS, JS"],
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #project2
-        id: "project2",
-        name: "Project 2",
-        skills: ["HTML, CSS, JS"],
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #project3
-        id: "project3",
-        name: "Project 3",
-        skills: ["HTML, CSS, JS"],
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #project4
-        id: "project4",
-        name: "Project 4",
-        skills: ["HTML, CSS, JS"],
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #project5
-        id: "project5",
-        name: "Project 5",
-        skills: ["HTML, CSS, JS"],
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #project6
-        id: "project6",
-        name: "Project 6",
-        skills: ["HTML, CSS, JS"],
-        url: "https://github.com/kaustubhai"
-    },
-]
-// Edit your Miscellaneous Activities, its name and the url. 
+  {
+    // Add image in './styles/images.css' in #project1
+    id: "project1",
+    name: "Project 1",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project2
+    id: "project2",
+    name: "Project 2",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project3
+    id: "project3",
+    name: "Project 3",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project4
+    id: "project4",
+    name: "Project 4",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project5
+    id: "project5",
+    name: "Project 5",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #project6
+    id: "project6",
+    name: "Project 6",
+    skills: ["HTML, CSS, JS"],
+    url: "https://github.com/kaustubhai",
+  },
+];
+// Edit your Miscellaneous Activities, its name and the url.
 // You can omit freely anything if you dont have it
 // To Add a Activity, copy any one below and paste it after the last comma and increment the id's Miscellaneous number
-const section4Title = "Miscellaneous"
+const section4Title = "Miscellaneous";
 const miscellaneous = [
-    {
-        // Add image in './styles/images.css' in #misc1
-        id: "misc1",
-        name: "Miscellaneous 1",
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #misc2        
-        id: "misc2",
-        name: "Miscellaneous 2",
-        url: "https://github.com/kaustubhai"
-    },
-    {
-        // Add image in './styles/images.css' in #misc3
-        id: "misc3",
-        name: "Miscellaneous 3",
-        url: "https://github.com/kaustubhai"
-    },
-]
+  {
+    // Add image in './styles/images.css' in #misc1
+    id: "misc1",
+    name: "Miscellaneous 1",
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #misc2
+    id: "misc2",
+    name: "Miscellaneous 2",
+    url: "https://github.com/kaustubhai",
+  },
+  {
+    // Add image in './styles/images.css' in #misc3
+    id: "misc3",
+    name: "Miscellaneous 3",
+    url: "https://github.com/kaustubhai",
+  },
+];
 // Contact form text, and Jotforms link
 // To get your own jotform link, go to https://www.jotform.com/
 // If you hacve the link already, paste it in the contactUrl below
-const section5Title = "Get in Touch"
+const section5Title = "Get in Touch";
 const contact = {
-    pitch: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.",
-    copyright: "Kaustubh Mishra",
-    contactUrl: ''
-}
+  pitch:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.",
+  copyright: "Kaustubh Mishra",
+  contactUrl: "",
+};
 // Paste your respective social media links. You can omit any if you dont have it
 // Upload your resume in your drive, get the shaareable link and paste it in the resume section
 const social = {
-    github: 'https://github.com',
-    facebook: 'https://facebook.com',
-    linkedin: 'https://linkedin.com',
-    instagram: 'https://instagram.com',
-    resume: 'https://novoresume.com/'
-}
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  instagram: "https://instagram.com",
+  resume: "https://novoresume.com/",
+};
 // Dont change anything here
-export { animation, header, background, about, skillsBar, projects, miscellaneous, contact, social, section2title, section3Title, section4Title, section5Title }
+export {
+  animation,
+  header,
+  background,
+  about,
+  skillsBar,
+  projects,
+  miscellaneous,
+  contact,
+  social,
+  section2title,
+  section3Title,
+  section4Title,
+  section5Title,
+};
