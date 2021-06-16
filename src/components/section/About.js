e77172f9ec3345f7094b114c1df45709cb4e368b
kaustubh <kaustubh229@gmail.com>
2021-06-17 02:52:37 +0530

    Optimisations: Dark mode & Firebase config files removed

--- src/components/section/About.js
+++ src/components/section/About.js
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div id="about" className="effect2">
             <div data-aos="zoom-in-up" data-aos-once="true" className="row">
-            <div className="col-12 d-none d-md-block offset-md-1 col-md-4 Photo" id="not"></div>
+            <div className="col-12 d-none d-md-block offset-md-1 col-md-4 Photo" id="not-dark2"></div>
             <div className="col-12 offset-md-1 col-md-6 about">
                 <div className="About-title-box">
                 <h1 id="About" className="red-line">{section2title}</h1>
--- src/components/section/Header.js
+++ src/components/section/Header.js
@@ -12,13 +12,13 @@ const Header = () => {
         })
     }
 
-    const toggleDarkMode = () =>  {
+    const toggleDarkMode = (e) =>  {
         document.documentElement.classList.toggle('dark-mode')
         document.getElementById('not-dark').classList.toggle('inverse-dark')
-        document.getElementById('not').classList.toggle('inverse-dark')
+        document.getElementById('not-dark2').classList.toggle('inverse-dark')
         var x = document.getElementsByClassName('img-pro')
         for(let i = 0; i < x.length; i += 1) {
-          x.item(i).classList.toggle("inverse-dark");
+            x.item(i).classList.toggle("inverse-dark");
         }
         
         if (document.documentElement.classList.contains('dark-mode'))
@@ -33,7 +33,7 @@ const Header = () => {
                 <h1>{ `I'm ${header.name}` }</h1>
             <p className="line-1 anim-typewriter">and this is my portfolio... </p>
             <label className="switch">
-                <input id="mode-switch" onClick={toggleDarkMode} type="checkbox"/>
+                <input id="mode-switch" onClick={e => toggleDarkMode(e)} type="checkbox"/>
                 <span className="slider round"></span>
             </label>
             <HeaderButton/>
